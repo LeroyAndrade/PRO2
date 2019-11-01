98e60523c243a947f54ad56ae27d76b343b6cf89
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2019-11-01 01:17:16 +0100

    Errors opgelost: CS1002, CS1022, CS8124, CS1026, CS0116, CS1031
    Toegevoegd: accolades, dubbelepunt comma's en de code afgesloten waar het moest
    - de if statements aangevuld en korter gemaakt.
    - Welkomst bericht aangepast omdat er een specifiek dagdeel werd benoemd waarmee de gast verkeerd begroet zal worden op een later dagdeel.

--- wirwar/Program.cs
+++ wirwar/Program.cs
@@ -6,21 +6,32 @@ namespace wirwar
     {
         static void Main(string[] args)
         {
-            String name = Console.ReadLine();
-            Console.WriteLine("Wat is je naam?");           
-
-            if(name == "Erwin"){                
-                Console.WriteLine("Goedemorgen wie ben jij eigenlijk?");
-                
-            }else(name == "Erik")
-                Console.WriteLine("Goedemorgen Alex");
-                Console.WriteLine("goedemorgen Erik");}}}
-            else if(name=="Alex"){
-                Console.WriteLine("goedemorgen Erwin!");
-                
-            }else if{{}
-                
-            }
+        if(name == "Erwin"){Console.WriteLine("Welkom Erwin");}
+            else if(name == "Erik"){Console.WriteLine("Welkom Erik");}
+            else if(name=="Alex"){Console.WriteLine("Welkom Alex");}
+            else if(name =="Leroy"){Console.WriteLine("Welkom Leroy");}
+        else Console.WriteLine("Ongeldig invoer, neemt contact op met de administrator om het probleem op te lossen.");
         }
     }
+}
+
+    /* opgeloste errors door Leroy Andrade
+        Program.cs(15,34): error CS1002: ; expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(18,13): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(18,25): error CS8124: Tuple must contain at least two elements. [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(18,25): error CS1026: ) expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(18,25): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(19,25): error CS0116: A namespace cannot directly contain members such as fields or methods [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(19,35): error CS1031: Type expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(19,35): error CS8124: Tuple must contain at least two elements. [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(19,35): error CS1026: ) expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(19,35): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(21,13): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(21,14): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(21,23): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(23,13): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(24,9): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(25,5): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+        Program.cs(26,1): error CS1022: Type or namespace definition, or end-of-file expected [D:\Mega\Github\AMC\PRO2\wirwar\wirwar.csproj]
+     */
 }
